4bf17749cf773ca724699bb25d7b99dda223e6d9
omeryancii <YOUR_EMAILomer.yanci@teamdefinex.com>
2025-09-26 10:57:45 +0300

    lab3 tests

--- Exercise/lab3-tdd-kata-warmup-bank/src/main/java/example/StringCalculator.java
+++ Exercise/lab3-tdd-kata-warmup-bank/src/main/java/example/StringCalculator.java
@@ -2,6 +2,22 @@ package example;
 
 public class StringCalculator {
     public int add(String input) {
-        throw new UnsupportedOperationException("Implement me via TDD");
+    try{
+        if(input.length()>=2){
+        String[] strArray = input.split(",");
+        int total = 0;
+        for(int i = 0; i <= strArray.length -1; i++){
+            total = total + Integer.parseInt(strArray[i]);
+        }
+        return total;
+        }
+        else if(input.length() == 1){
+            return Integer.parseInt(input);
+        }
+        return 0;
+    }
+    catch(UnsupportedOperationException e){
+        throw e;
+        }
     }
 }
--- Exercise/lab3-tdd-kata-warmup-bank/src/main/java/lab3/BankAccount.java
+++ Exercise/lab3-tdd-kata-warmup-bank/src/main/java/lab3/BankAccount.java
@@ -4,11 +4,23 @@ public class BankAccount {
     private int balance = 0;
 
     public void deposit(int amount) {
+        if(amount < 0)
         throw new UnsupportedOperationException("TDD: implement deposit");
+        else
+        balance = balance + amount;
     }
 
     public void withdraw(int amount) {
+        if(amount < 0)
         throw new UnsupportedOperationException("TDD: implement withdraw");
+        else{
+            if(balance < amount){
+                throw new UnsupportedOperationException("yetersiz");
+            }
+            else{
+                balance = balance - amount;
+            }
+        }
     }
 
     public int getBalance() {
